cab83597ab07233e55e5791a68bcec594b1eb2a0
AmiltonMn <amilton.moreiran@hotmail.com>
2025-03-13 13:39:40 -0300

    Arrumando algumas coisas de estrutura

--- Poke/Backend/Src/Services/ApiService.ts
+++ Poke/Backend/Src/Services/ApiService.ts
@@ -10,6 +10,15 @@ class ApiService {
             console.log(`Ocorreu o seguinte erro ao pegar os dados do pokemon: ${error}`)
         }
     }
+
+    static async GetAllPokemons() : Promise<any> {
+
+        try {
+            const response = await axios.get(`${APIURL}/pokemon`)
+        } catch (error) {
+            console.log(`Ocorreu um erro ao pegar os dados: ${error}`)
+        }
+    }
 }
 
 export default ApiService;
